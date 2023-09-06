79c47f9c6c29f7f43148c9d422404091a2134ca4
Otavio Augusto Abati <otavioabatii@gmail.com>
2023-09-06 17:04:07 -0300

    Implementação dos recursos de interação em tela!

--- lista_mercado/js/index.js
+++ lista_mercado/js/index.js
@@ -1,21 +1,61 @@
 const cardContent = window.document.getElementsByClassName("card-content")[0];
 var countIndex =0;
 
+function deleteComponent(index){
+    const itemDelete = window.document.getElementById(index);
+    itemDelete.outerHTML = "";
+}
+
+function onChecked(index){
+    const itemChecked = window.document.getElementById(index);
+    const itemRad = itemChecked.getElementsByClassName("item-rad")[0];
+    const span = itemRad.getElementsByTagName("span")[0];
+    const contentInput = itemChecked.getElementsByClassName("card-content-input")[0];
+    
+    const isExistElemnt = itemChecked.getElementsByClassName("item-rad-checked");
+    const input = itemChecked.getElementsByTagName("input")[0];
+
+    if(isExistElemnt.length <=0){
+        //CHECKED
+        if(input.value.trim() !== ""){
+        itemRad.setAttribute("class", "item-rad item-rad-checked");
+        contentInput.setAttribute("class", "card-content-input card-content-input-checked");
+        span.innerHTML = "check_circle";
+        }
+    }else{
+        //UNCHECKED
+    itemRad.setAttribute("class", "item-rad");
+    contentInput.setAttribute("class", "card-content-input");
+    span.innerHTML = "radio_button_unchecked";
+    }
+}
+
 function addItem(){
-    const item = window.document.getElementById(countIndex);
-    console.log(countIndex, item);
+    const item = window.document.getElementById(countIndex-1);
+
+    if (item) {
+        const input = item.getElementsByTagName("input")[0];
+        if (input.value.trim() !== "") {
+            appendComponent();
+        }
+    }
+    else{
     appendComponent();
+    }
 }
 
 console.log(cardContent);
 
 function appendComponent() {
+    //CONSTROI LI
     var li = window.document.createElement("li");
     li.setAttribute("id", countIndex++);
     li.setAttribute("class","card-content-item");
 
+    //CONSTROI DEL
     var btnClose = window.document.createElement("div");
     btnClose.setAttribute("class","item-del");
+    btnClose.setAttribute("onClick", `deleteComponent(${(countIndex-1)})`)
 
     li.appendChild(btnClose);
     
@@ -30,19 +70,29 @@ function appendComponent() {
     var itemDescription = window.document.createElement("div");
     itemDescription.setAttribute("class", "item-description");
 
-    //CONSTROI INPUT
+    
     var cardContentInput = window.document.createElement("div");
     cardContentInput.setAttribute("class", "card-content-input");
 
+    //CONSTROI INPUT
     var input = window.document.createElement("input");
+
+    input.addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+            console.log("teste");
+            addItem();
+        }
+    });
     
     cardContentInput.appendChild(input);
     itemDescription.appendChild(cardContentInput);
     li.appendChild(itemDescription);
 
-    //CONSTROI ITEM RAD
+    //CONSTROI DIV ITEM RAD
     var itemRad = window.document.createElement("div");
     itemRad.setAttribute("class","item-rad");
+
+    itemRad.setAttribute("onClick", `onChecked(${(countIndex-1)})`)
 
     var spanChecked = window.document.createElement("span");
     spanChecked.setAttribute("class","material-symbols-outlined");
